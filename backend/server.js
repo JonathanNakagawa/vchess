5c46ffad1e5f95ed0e0b0a2ba493bdcebf23f66a
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-09-25 23:48:26 -0400

    Created initial server setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+// Set up middleware
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+// Read values from .env
+port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+// Connect to database
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Database connection successful'))
+    .catch(err => console.log('Database connection failed:' + err));
+
+app.listen(port, () => console.log('Server running on port ' + port));
