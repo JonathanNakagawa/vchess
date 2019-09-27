b1115942fffeb25cdb7995c7571760ed2317004f
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-09-26 22:22:11 -0400

    Fixed bug. Shouldn't be using cors on all routes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 // Set up middleware
-app.use(cors);
+// app.use(cors);
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(passport.initialize());
